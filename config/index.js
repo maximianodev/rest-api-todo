be71becb9c9d15409306f17abdba8a5534e5fa7b
lucas <lucas97lopes@hotmail.com>
2020-12-13 11:18:53 -0300

    adjustment ✔�

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ const app = require('../routes/app');
 const port = process.env.PORT || 3332;
 
 const server = app.listen(port, function () {
-  console.log(`Express server listening on port  ${port}`);
+  console.log(`Express server listening on port http://localhost:${port}/todos`);
 });
--- routes/app.js
+++ routes/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const db = require('../config/mongoConnection');
 
+const cors = require('cors');
+app.use(cors());
+
 const TodoController = require('../controllers/TodoController');
 app.use('/todos', TodoController);
 
